db0fd0283f4d7fc032ae614cec8db97c54b9af97
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2026-09-19 02:01:57 -0500

    Register Discord cosmetics in the deployment migration history

---
+++ packages/db/drizzle/0001_discord_cosmetics.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `user` ADD `discordBannerUrl` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `discordAccentColor` integer;--> statement-breakpoint
+ALTER TABLE `user` ADD `discordAvatarDecorationUrl` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `discordGuildTag` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `discordGuildBadgeUrl` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `discordNameplateUrl` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `discordPublicFlags` integer;--> statement-breakpoint
+ALTER TABLE `user` ADD `discordProfileSyncedAt` integer;--> statement-breakpoint
+ALTER TABLE `user` ADD `discordProfileRevision` text;
